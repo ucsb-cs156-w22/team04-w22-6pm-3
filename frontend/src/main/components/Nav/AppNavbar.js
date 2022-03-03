a166799c2ffa1220345dc346800465aedc19ee8a
DannyCoco <72511504+DannyCoco@users.noreply.github.com>
2022-03-02 23:36:56 -0800

    dc - editCommons page placeholder

--- frontend/src/main/components/Nav/AppNavbar.js
+++ frontend/src/main/components/Nav/AppNavbar.js
@@ -47,6 +47,7 @@ export default function AppNavbar({ currentUser, systemInfo, doLogout, currentUr
                 hasRole(currentUser, "ROLE_ADMIN") && (
                   <NavDropdown title="Admin" id="appnavbar-admin-dropdown" data-testid="appnavbar-admin-dropdown" >
                     <NavDropdown.Item href="/admin/createcommons">Create Commons</NavDropdown.Item>
+                    <NavDropdown.Item href="/admin/editcommons/:id">Edit Commons</NavDropdown.Item>
                     <NavDropdown.Item href="/admin/users">Users</NavDropdown.Item>
                   </NavDropdown>
                 )
---
+++ frontend/src/main/pages/AdminEditCommonsPage.js
@@ -0,0 +1,22 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+/*
+import { useParams } from "react-router-dom";
+import CreateCommonsForm from "main/components/Commons/CreateCommonsForm";
+import { Navigate } from 'react-router-dom'
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify"
+
+import { useBackendMutation } from "main/utils/useBackend";
+*/
+export default function AdminEditCommonsPage() {
+    return (
+      <BasicLayout>
+        <div className="pt-2">
+          <h1>Edit Commons</h1>
+          <p>
+            This is where the edit page will go
+          </p>
+        </div>
+      </BasicLayout>
+    )
+  }
