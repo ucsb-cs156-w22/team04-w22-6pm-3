1a3f164d20413f9609ab2e8f26c1cd385a2b0216
DannyCoco <72511504+DannyCoco@users.noreply.github.com>
2022-03-08 18:52:32 -0800

    dc - Edit Commons Page

--- frontend/src/main/pages/AdminEditCommonsPage.js
+++ frontend/src/main/pages/AdminEditCommonsPage.js
@@ -1,14 +1,73 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
-/*
 import { useParams } from "react-router-dom";
 import CreateCommonsForm from "main/components/Commons/CreateCommonsForm";
 import { Navigate } from 'react-router-dom'
 import { useBackend, useBackendMutation } from "main/utils/useBackend";
 import { toast } from "react-toastify"
 
-import { useBackendMutation } from "main/utils/useBackend";
-*/
 export default function AdminEditCommonsPage() {
+  let { id } = useParams();
+
+  const { data: common, error: error, status: status } =
+    useBackend(
+      [`/api/commons?id=${id}`],
+      {
+        method: "GET",
+        url: `/api/commons`,
+        params: {
+          id
+        }
+      }
+    );
+
+    const objectToAxiosPutParams = (common) => ({
+      url: "/api/commons",
+      method: "PUT",
+      params: {
+        id: common.id,
+      },
+      data: {
+       name: common.name,
+       cowPrice: common.cowPrice,
+       milkPrice: common.milkPrice,
+       startingBalance: common.startingBalance,
+       startingDate: common.startingDate
+      }
+    });
+
+    const onSuccess = (common) => {
+      toast(`Common Updated - id: ${common.id} name: ${common.name}`)
+    }
+
+    const mutation = useBackendMutation(
+      objectToAxiosPutParams,
+      { onSuccess },
+      [`/api/commons?id=${id}`]
+    );
+
+    const { isSuccess } = mutation
+
+    const onSubmit = async (data) => {
+      mutation.mutate(data);
+    }
+
+    if (isSuccess) {
+      return <Navigate to="/commons/list" />
+    }
+
+    return(
+      <BasicLayout>
+        <div className = "pt-2">
+          <h1>Edit Common</h1>
+          {common &&
+            <CreateCommonsForm initialCommon={common} submitAction={onSubmit} buttonLabel="Update" />
+          }  
+        </div>
+      </BasicLayout>
+    )
+}
+
+/*
     return (
       <BasicLayout>
         <div className="pt-2">
@@ -20,3 +79,4 @@ export default function AdminEditCommonsPage() {
       </BasicLayout>
     )
   }
+  */
